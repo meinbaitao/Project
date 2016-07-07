ed5c9e032b3a051dcd3574ab50e22b886d340873
mydyl <dyl@clearcom.com.cn>
2016-07-07 10:48:30 +0800

    add new project shiro_ld

--- shiro_ld/src/main/java/gz/dyl/shiro_ld/controller/ItemController.java
+++ shiro_ld/src/main/java/gz/dyl/shiro_ld/controller/ItemController.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping(value = "item")
+@RequestMapping(value = "item")   
 public class ItemController {
 
 	@RequiresPermissions(value = "item:insert")
